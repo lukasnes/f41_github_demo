a228cafbb1d897576fdf2ec4960bd19a79a7efaf
Lukas Nesittka <lukasnesittka25@gmail.com>
2023-05-16 11:35:15 -0500

    Initial commit

---
+++ helloWorld.js
@@ -0,0 +1,5 @@
+// camelCase -- JavaScript: helloWorld, numGen, isTrue
+// snake_case -- Python: hello_world, num_gen, is_true
+// kebab-case -- HTML & CSS: hello-world, num-gen, is-true
+
+console.log("Hello World!")
